14b80ffd67910bcd93d60d5daa6b5b0812a70bfb
Nicolas Bouvie da Silva <nicolas.bouviedasilva@bunge.com>
2019-02-01 16:46:25 +0100

    Ignore certificates from server

--- src/main/java/me/bouvie/wasconfigmanager/WasConfigManagerApplication.java
+++ src/main/java/me/bouvie/wasconfigmanager/WasConfigManagerApplication.java
@@ -1,5 +1,6 @@
 package me.bouvie.wasconfigmanager;
 
+import me.bouvie.wasconfigmanager.factory.IgnoreSSLSocketFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +10,7 @@ import java.security.Security;
 public class WasConfigManagerApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("javax.net.ssl.trustStore", WasConfigManagerApplication.class.getResource("/trust.jks").getFile());
-		System.setProperty("javax.net.ssl.trustStorePassword", "WebAS!");
-        Security.setProperty("ssl.SocketFactory.provider", "com.ibm.jsse2.SSLSocketFactoryImpl");
-        Security.setProperty("ssl.ServerSocketFactory.provider", "com.ibm.jsse2.SSLServerSocketFactoryImpl");
+        Security.setProperty("ssl.SocketFactory.provider", IgnoreSSLSocketFactory.class.getName());
 
 		SpringApplication.run(WasConfigManagerApplication.class, args);
 	}
---
+++ src/main/java/me/bouvie/wasconfigmanager/factory/IgnoreSSLSocketFactory.java
@@ -0,0 +1,67 @@
+package me.bouvie.wasconfigmanager.factory;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.cert.X509Certificate;
+
+public class IgnoreSSLSocketFactory extends SSLSocketFactory {
+
+    private final SSLSocketFactory factory;
+
+    public IgnoreSSLSocketFactory() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+        }};
+
+        // Install the all-trusting trust manager
+        SSLContext sc = SSLContext.getInstance("TLS");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+
+        factory = sc.getSocketFactory();
+    }
+
+    @Override
+    public String[] getDefaultCipherSuites() {
+        return factory.getDefaultCipherSuites();
+    }
+
+    @Override
+    public String[] getSupportedCipherSuites() {
+        return factory.getSupportedCipherSuites();
+    }
+
+    @Override
+    public Socket createSocket(Socket socket, String s, int i, boolean b) throws IOException {
+        return factory.createSocket(socket, s, i, b);
+    }
+
+    @Override
+    public Socket createSocket(String s, int i) throws IOException, UnknownHostException {
+        return factory.createSocket(s, i);
+    }
+
+    @Override
+    public Socket createSocket(String s, int i, InetAddress inetAddress, int i1) throws IOException, UnknownHostException {
+        return factory.createSocket(s, i, inetAddress, i1);
+    }
+
+    @Override
+    public Socket createSocket(InetAddress inetAddress, int i) throws IOException {
+        return factory.createSocket(inetAddress, i);
+    }
+
+    @Override
+    public Socket createSocket(InetAddress inetAddress, int i, InetAddress inetAddress1, int i1) throws IOException {
+        return factory.createSocket(inetAddress, i, inetAddress1, i1);
+    }
+}
